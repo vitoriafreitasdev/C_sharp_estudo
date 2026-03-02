e0961416027f37d643baca550134eed6acaeee24
Vitória Freitas <vitoriafreitas110903@gmail.com>
2026-03-02 19:12:30 -0300

    Primeiro arquivo add

---
+++ primeiro_console_proejto/primeiro_console_proejto/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace Exemplo
+{
+    class Program
+    {
+        static void Main()
+        {
+            // Imprime “Olá, Mundo!” no console
+            Console.WriteLine("Olá, mundo!");
+        }
+    }
+}
